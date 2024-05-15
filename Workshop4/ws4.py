5fd04eb529f2f39129b153461408a8a389a2fe84
MrIanis <ianis.pouru@gmail.com>
2024-05-15 11:51:26 +0200

    ajout du tri par fréquence et par ordre alphabetique

--- Workshop4/ws4.py
+++ Workshop4/ws4.py
@@ -1,24 +1,33 @@
 import string
 
-# Définissez le texte à analyser
+# Définir le texte à analyser
 texte = "afrgthykighlompfdshygtfghygjkhlfdkshygtfhybgjukilofm"
 
-# Supprimez les ponctuations et convertissez le texte en minuscules
+# Supprimer les ponctuations et convertir le texte en minuscules
 texte_nettoye = texte.translate(str.maketrans("", "", string.punctuation)).lower().replace(" ", "")
 
-# Créez un dictionnaire pour stocker les fréquences des lettres
+# Créer un dictionnaire pour stocker les fréquences des lettres
 frequences = {}
 
-# Calculez les fréquences des lettres
+# Calculer les fréquences des lettres
 for lettre in texte_nettoye:
     if lettre in frequences:
         frequences[lettre] += 1
     else:
         frequences[lettre] = 1
 
-# Trouvez la fréquence maximale
-freq_max = max(frequences.values())
+# Demander à l'utilisateur de choisir l'ordre d'affichage
+tri = input("Choisissez l'ordre d'affichage (A pour alphabétique, F pour fréquence): ")
 
-# Affichez les résultats
-for lettre, freq in frequences.items():
+# Trier le dictionnaire selon l'ordre choisi
+if tri.upper() == "A":
+    frequences_triees = dict(sorted(frequences.items()))
+else:
+    frequences_triees = dict(sorted(frequences.items(), key=lambda x: x[1], reverse=True))
+
+# Trouver la fréquence maximale
+freq_max = max(frequences_triees.values())
+
+# Afficher les résultats
+for lettre, freq in frequences_triees.items():
     print(lettre, "|", "■" * int((freq / freq_max) * 40), "|", freq)
